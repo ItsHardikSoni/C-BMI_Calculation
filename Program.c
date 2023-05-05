58e01e3fe56a570b61b16d1c8941d75d7b8d98ff
Hardik Soni <131765546+ItsHardikSoni@users.noreply.github.com>
2023-05-05 20:01:51 +0530

    Update Program.c

--- Program.c
+++ Program.c
@@ -16,30 +16,38 @@ Morbidly Obese          >=40   */
 int main()
 {
     float bmi, height, weight;
-    printf("\n Enter Height (in metere) and Weight (in kilogram) of a Persion : ");
+    printf("\n Enter Height (in metere) and Weight (in kilogram) of a Person : ");
     scanf("%f %f", &height, &weight);
     bmi = weight/(height*height);
-    printf("BMI of the Persion = %f\n",bmi);
+     printf("BMI of the Person = %f\n",bmi);
 
     if (bmi>0 && bmi<=15)
+    {
         printf("starvation \n");
-
+    }
     else if (bmi>=15.1 && bmi<=17.5)
+    {
         printf("Anorexic \n");
-
-    else if (bmi>17.6 && bmi<=18.5)
+    }
+    else if (bmi>=17.6 && bmi<=18.5)
+    {
         printf("Under Weight \n");
-
-    else if (bmi>18.6 && bmi<=24.9)
+    }
+    else if (bmi>=18.6 && bmi<=24.9)
+    {
         printf("Ideal \n");
-
-    else if (bmi>25 && bmi<=25.9)
+    }    
+    else if (bmi>=25 && bmi<=25.9)
+    {
         printf("Over Weight \n");
-
-    else if (bmi>30 && bmi<=30.9)
-        printf("Obses \n");
-
+    }
+    else if (bmi>=30 && bmi<=30.9)
+    {
+       printf("Obses \n");
+    }
     else if (bmi>=40)
+    {
         printf("Morbidly Obese \n");
+    }
     return 0;
 }
